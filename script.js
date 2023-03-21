c0b6bdf4035a8d768d18eb15aec87154097afe68
Joshua Sigston <joshua.sigston@outlook.com>
2023-03-21 16:09:11 -0600

    re-write

--- script.js
+++ script.js
@@ -1,24 +1,7 @@
-// mobile nav menu
+/* mobile navigation */
 const hamburger = document.getElementById('hamburger')
-const pattyOne = document.getElementById('patty-one')
-const pattyTwo = document.getElementById('patty-two')
-const pattyThree = document.getElementById('patty-three')
-const nav = document.getElementById('nav')
-
-const navItem = document.querySelectorAll('.nav_item')
+const mobileNav = document.getElementById('mobile-nav')
 
 hamburger.addEventListener('pointerdown', () => {
-    pattyOne.classList.toggle('shift_right')
-    pattyThree.classList.toggle('shift_right')
-
-    pattyTwo.classList.toggle('shift_left')
-
-    nav.classList.toggle('nav_show')
-})
-
-navItem.forEach( (item) => {
-    item.addEventListener( 'pointerdown', () => {
-        console.log(item)
-        nav.classList.toggle('nav_show')
-    })
+    mobileNav.classList.toggle('toggle_nav')
 })
